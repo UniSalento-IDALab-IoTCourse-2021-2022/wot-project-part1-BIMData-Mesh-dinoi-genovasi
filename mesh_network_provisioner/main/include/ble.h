34655457856342c0f0b1ec2811be42faf9928b1e
Gabriele Tramonte <gabry.t92@gmail.com>
2021-12-22 16:36:15 +0100

    #IoT-15 ibeacon scan

--- mesh_network_provisioner/main/include/ble.h
+++ mesh_network_provisioner/main/include/ble.h
@@ -4,8 +4,43 @@
 #include <esp_bt.h>
 #include <esp_log.h>
 #include <esp_bt_main.h>
+#include <esp_gap_ble_api.h>
+#include "esp_ibeacon_api.h"
+
 #define BLE_TAG "BLE"
+#define REMOTE_SERVICE_UUID        0x00FF
+#define REMOTE_NOTIFY_CHAR_UUID    0xFF01
+#define INVALID_HANDLE   0
+
+//static const char remote_device_name[] = "ESP_GATTS_DEMO";
+static bool connect    = false;
+static bool get_server = false;
+
+/* Declare static functions */
+static void esp_gap_cb(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t *param);
+
+static esp_bt_uuid_t remote_filter_service_uuid = {
+        .len = ESP_UUID_LEN_16,
+        .uuid = {.uuid16 = REMOTE_SERVICE_UUID,},
+};
+
+static esp_bt_uuid_t remote_filter_char_uuid = {
+        .len = ESP_UUID_LEN_16,
+        .uuid = {.uuid16 = REMOTE_NOTIFY_CHAR_UUID,},
+};
+
+static esp_ble_scan_params_t ble_scan_params = {
+        .scan_type              = BLE_SCAN_TYPE_ACTIVE,
+        .own_addr_type          = BLE_ADDR_TYPE_PUBLIC,
+        .scan_filter_policy     = BLE_SCAN_FILTER_ALLOW_ALL,
+        .scan_interval          = 0x50,
+        .scan_window            = 0x30,
+        .scan_duplicate         = BLE_SCAN_DUPLICATE_DISABLE
+};
 
 esp_err_t bluetooth_init(void);
+
+static void esp_gap_cb(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t *param);
+
 
 #endif //TEST_MESH_NETWORK_BLE_H
