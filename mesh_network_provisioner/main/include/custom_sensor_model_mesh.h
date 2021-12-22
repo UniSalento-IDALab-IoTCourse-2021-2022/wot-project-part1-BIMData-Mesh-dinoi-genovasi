1188ed186a5ab45b20850546113d0f2c853a21b2
Gabriele Tramonte <gabry.t92@gmail.com>
2021-12-22 13:45:08 +0100

    #IoT-11 fix device_name length

--- mesh_network_provisioner/main/include/custom_sensor_model_mesh.h
+++ mesh_network_provisioner/main/include/custom_sensor_model_mesh.h
@@ -18,7 +18,7 @@ typedef struct __attribute__((packed)) {
     float lux;
     int temperature;
     int humidity;
-    char device_name[6];
+    char device_name[7];
 } model_sensors_data_t;
 
 #endif //TEST_MESH_NETWORK_CUSTOM_SENSOR_MODEL_MESH_H
