6a7997a1c7881a149cda04bc4a284769920adcb8
Gabriele Tramonte <gabry.t92@gmail.com>
2022-01-21 18:13:21 +0100

    #IoT-13 code clean

--- mesh_network_provisioner/main/lib/triangulation.c
+++ mesh_network_provisioner/main/lib/triangulation.c
@@ -25,28 +25,6 @@ void estimate_position(){
         ((a[0] - a[1]) * (b[1] - b[2]) - (a[1] - a[2]) * (b[0] - b[1]));
     y = ((c[2] - c[1]) * (a[0] - a[1]) - (c[1] - c[0]) * (a[1] - a[2])) /
         ((a[0] - a[1]) * (b[1] - b[2]) - (a[1] - a[2]) * (b[0] - b[1]));
-    /*float A = 2*fixed_nodes[1].x - 2*fixed_nodes[0].x;
-    float B = 2*fixed_nodes[1].y - 2*fixed_nodes[0].y;
-    float C = fixed_nodes[0].distance*fixed_nodes[0].distance - fixed_nodes[1].distance*fixed_nodes[1].distance - fixed_nodes[0].x*fixed_nodes[0].x +
-            fixed_nodes[1].x*fixed_nodes[1].x - fixed_nodes[0].y * fixed_nodes[0].y + fixed_nodes[1].y*fixed_nodes[1].y;
-    float D = 2*fixed_nodes[2].x - 2*fixed_nodes[1].x;
-    float E = 2*fixed_nodes[2].y- 2*fixed_nodes[1].y;
-    float F =  fixed_nodes[1].distance*fixed_nodes[1].distance -  fixed_nodes[2].distance*fixed_nodes[2].distance - fixed_nodes[1].x*fixed_nodes[1].x +
-            fixed_nodes[2].x*fixed_nodes[2].x - fixed_nodes[1].y*fixed_nodes[1].y + fixed_nodes[2].y*fixed_nodes[2].y;
-    x = (C*E - F*B) / (E*A - B*D);
-    y = (C*D - A*F) / (B*D - A*E);
-
-    if( x >= 1.8f){
-        ESP_LOGW("TRILAT","After the table");
-    }else if (x <0){
-        ESP_LOGW("TRILAT", "Before the table");
-    }
-
-    if (y > 1.6f){
-        ESP_LOGW("TRILAT","After the table");
-    } else if (y < 0){
-        ESP_LOGW("TRILAT","Before the table");
-    }*/
 
     ESP_LOGI("TRILAT", "Estimated Beacon position: X: %f, Y: %f", x, y);
     mqtt_publish(x, y);
