5b3385c0ba837ed2557feb13a8aa6897add3cec5
Gabriele Tramonte <gabry.t92@gmail.com>
2022-01-21 15:47:05 +0100

    #IoT-13 Changed trilateration algorithm

--- mesh_network_provisioner/main/lib/triangulation.c
+++ mesh_network_provisioner/main/lib/triangulation.c
@@ -12,17 +12,20 @@
 }
 
 void estimate_position(){
-    //float a[3],b[3],c[3];
+    float a[3], b[3], c[3];
     float x;
     float y;
-    /*for(int i=0 ; i<3; i++){
-        a[i]=-2*fixed_nodes[i].x;
-        b[i]=-2*fixed_nodes[i].y;
-        c[i]=fixed_nodes[i].x*fixed_nodes[i].x + fixed_nodes[i].y*fixed_nodes[i].y - fixed_nodes[i].distance*fixed_nodes[i].distance;
-    }*/
-   /* x = ((c[3] - c[2])*(b[1]-b[3]) - (c[3]-c[1])*(b[2]-b[3]))/((a[2]-a[3])*(b[1]-b[3]) - (a[1]-a[3])*(b[2]-b[3]));
-    y = (-(c[3]-c[2])*(a[1]-a[3]))/((a[2]-a[3])*(b[1]-b[3]) - (a[1]-a[3])*(b[2]-b[3]));*/
-    float A = 2*fixed_nodes[1].x - 2*fixed_nodes[0].x;
+    for (int i = 0; i < 3; i++) {
+        a[i] = -2 * fixed_nodes[i].x;
+        b[i] = -2 * fixed_nodes[i].y;
+        c[i] = fixed_nodes[i].x * fixed_nodes[i].x + fixed_nodes[i].y * fixed_nodes[i].y -
+               fixed_nodes[i].distance * fixed_nodes[i].distance;
+    }
+    x = ((c[1] - c[0]) * (b[1] - b[2]) - (c[2] - c[1]) * (b[0] - b[1])) /
+        ((a[0] - a[1]) * (b[1] - b[2]) - (a[1] - a[2]) * (b[0] - b[1]));
+    y = ((c[2] - c[1]) * (a[0] - a[1]) - (c[1] - c[0]) * (a[1] - a[2])) /
+        ((a[0] - a[1]) * (b[1] - b[2]) - (a[1] - a[2]) * (b[0] - b[1]));
+    /*float A = 2*fixed_nodes[1].x - 2*fixed_nodes[0].x;
     float B = 2*fixed_nodes[1].y - 2*fixed_nodes[0].y;
     float C = fixed_nodes[0].distance*fixed_nodes[0].distance - fixed_nodes[1].distance*fixed_nodes[1].distance - fixed_nodes[0].x*fixed_nodes[0].x +
             fixed_nodes[1].x*fixed_nodes[1].x - fixed_nodes[0].y * fixed_nodes[0].y + fixed_nodes[1].y*fixed_nodes[1].y;
@@ -33,6 +36,18 @@ void estimate_position(){
     x = (C*E - F*B) / (E*A - B*D);
     y = (C*D - A*F) / (B*D - A*E);
 
-    ESP_LOGI("TRILAT","Estimated Beacon position: X: %f, Y: %f",x,y);
-    mqtt_publish(x,y);
+    if( x >= 1.8f){
+        ESP_LOGW("TRILAT","After the table");
+    }else if (x <0){
+        ESP_LOGW("TRILAT", "Before the table");
+    }
+
+    if (y > 1.6f){
+        ESP_LOGW("TRILAT","After the table");
+    } else if (y < 0){
+        ESP_LOGW("TRILAT","Before the table");
+    }*/
+
+    ESP_LOGI("TRILAT", "Estimated Beacon position: X: %f, Y: %f", x, y);
+    mqtt_publish(x, y);
 }
