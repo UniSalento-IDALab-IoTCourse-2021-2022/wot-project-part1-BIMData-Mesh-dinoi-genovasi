4b762b52a4a44b3fa0507947204954fb3f77ed31
rickytau.developer <rickytau.developer@gmail.com>
2022-04-06 15:54:43 +0200

    #IoT-24 Add beacon-mesh sending loop

--- mesh_network_beacon/main/main.c
+++ mesh_network_beacon/main/main.c
@@ -41,5 +41,9 @@ void app_main(void)
 
     xTaskCreate(&ota_task, "ota_update_task", 8192, NULL, 5, NULL);
 
+    while(1){
+        ble_beacon_mesh_send();
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
 }
 
